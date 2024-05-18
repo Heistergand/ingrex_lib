c5c7f071cff7b7e0107716d5a2e15fe329a79f68
Heistergand <info@heistergand.com>
2024-05-18 08:51:27 +0200

    Update utils.py

--- ingrex/utils.py
+++ ingrex/utils.py
@@ -1,24 +1,65 @@
-# -*- coding:utf-8 -*-
 "Map Utils"
-from math import pi, sin, cos, tan, asin, radians, sqrt, log
+from math import pi, sin, cos, tan, asin, radians, sqrt, log, acos
 
-def calc_tile(lng, lat, zoomlevel):
+def calc_tile(lat, lng, zoomlevel):
     tilecounts = [1,1,1,40,40,80,80,320,1E3,2E3,2E3,4E3,8E3,16E3,16E3,32E3]
     rlat = radians(lat)
     tilecount = tilecounts[zoomlevel]
-    xtile = int((lng + 180.0) / 360.0 * tilecount)
-    ytile = int((1.0 - log(tan(rlat) + (1 / cos(rlat))) / pi) / 2.0 * tilecount)
-    return xtile, ytile
+    xTile = int((lng + 180.0) / 360.0 * tilecount)
+    yTile = int((1.0 - log(tan(rlat) + 1 / cos(rlat)) / pi) / 2.0 * tilecount)
+    return yTile, xTile
 
+def calc_field_to_tiles(field):
+    north = field['maxLatE6']
+    east = field['maxLngE6']
+    south = field['minLatE6']
+    west = field['minLngE6']
+    minxtile, minytile = calc_tile(north/1E6, west/1E6, 15)
+    maxxtile, maxytile = calc_tile(south/1E6, east/1E6, 15)
+    return minxtile, maxxtile, minytile, maxytile
+    
+    
 def calc_dist(lat1, lng1, lat2, lng2):
     lat1, lng1, lat2, lng2 = map(radians, [lat1, lng1, lat2, lng2])
+    # km = 6378.137 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1))
     dlat = lat1 - lat2
     dlng = lng1 - lng2
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlng/2)**2
     c = 2* asin(sqrt(a))
-    m = 6367.0 * c * 1000
-    return m
+    m = 6378.137 * c * 1000
+    # m = km * 1000
+    return round(m)
 
+def calc_dist_hires(lat1, lng1, lat2, lng2):
+    lat1, lng1, lat2, lng2 = map(radians, [lat1, lng1, lat2, lng2])
+    km = 6378.137 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lng2 - lng1))
+    # dlat = lat1 - lat2
+    # dlng = lng1 - lng2
+    # a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlng/2)**2
+    # c = 2* asin(sqrt(a))
+    # m = 6367.0 * c * 1000
+    m = km * 1000
+    return round(m)
+
+def trans_point_to_field(lat, lng, radius_km) :
+#todo
+    r = radius_km * 1000
+    a = 180 * radius_km / (6378.137 * pi);
+    minlat = lat - a
+    maxlat = lat + a
+    minlng = lng - a
+    maxlng = lng + a
+    field = {
+        'minLngE6':round(minlng * 1E6),
+        'minLatE6':round(minlat * 1E6),
+        'maxLngE6':round(maxlng * 1E6),
+        'maxLatE6':round(maxlat * 1E6),
+    }
+    return field
+        
+        
+    
+    
 def point_in_poly(x, y, poly):
     n = len(poly)
     inside = False
@@ -40,7 +81,7 @@ def transform(wgLat, wgLon):
     transform(latitude,longitude) , WGS84
     return (latitude,longitude) , GCJ02
     """
-    a = 6378245.0
+    a = 6378137.0
     ee = 0.00669342162296594323
     if (outOfChina(wgLat, wgLon)):
         mgLat = wgLat
@@ -78,7 +119,7 @@ def transformLon(x, y):
     ret += (20.0 * sin(x * pi) + 40.0 * sin(x / 3.0 * pi)) * 2.0 / 3.0
     ret += (150.0 * sin(x / 12.0 * pi) + 300.0 * sin(x / 30.0 * pi)) * 2.0 / 3.0
     return ret
-
+    
 def gcj02towgs84(lng, lat):
     """
     GCJ02(火星坐标系)转GPS84
